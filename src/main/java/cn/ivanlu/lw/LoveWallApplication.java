0ab492049182c8c0a9ebb3e5725e037f6a70e1c3
IvanLuLyf <835498692@qq.com>
2020-07-01 21:27:56 +0800

    START

---
+++ src/main/java/cn/ivanlu/lw/LoveWallApplication.java
@@ -0,0 +1,13 @@
+package cn.ivanlu.lw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoveWallApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoveWallApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/ivanlu/lw/LoveWallApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.ivanlu.lw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoveWallApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
